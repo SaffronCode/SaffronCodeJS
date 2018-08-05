a80f1bba7deada69f4c80977c001b5cb0e85fadd
MEbrahim Sepehr <ebrahimsepehr@gmail.com>
2018-08-05 15:47:54 +0430

    Page Manager and parameters

--- framework/pageManager/PageManager.js
+++ framework/pageManager/PageManager.js
@@ -1,32 +1,64 @@
-import EventDispatcher from "./EventDispatcher";
-import SimplePage from "./SimplePage";
 
-import JSFunctions from "../libs/JSFunctions" ;
+import PageData from "./PageData";
+import EventDispatcher from "../EventDispatcher";
 
+//import JSFunctions from "../libs/JSFunctions" ;
 
+const maximomAcceptableParamsOnURL = 10 ;
 
 var PageManager = {};
 PageManager.dispatcher = new EventDispatcher();
 PageManager.PAGE_CHANGED = "PAGE_CHANGED" ;
+
+PageManager.routerParamList = '' ;
+for(var i = 0 ; i<maximomAcceptableParamsOnURL ; i++)
+{
+    PageManager.routerParamList+='/:p'+i+'?';
+}
 
 ///Page list ↓
 PageManager.allPages = [] ;
 PageManager.menuPages = [] ;
 
 
-var lastPage = new SimplePage();
+var lastPage = new PageData();
 
-PageManager.changePage = function(targetPage,pageData={})
+PageManager.changePage = function(targetPage,pageData=[])
 {
     if(targetPage!==null)
     {
         lastPage = JSON.parse(JSON.stringify(targetPage)) ;
-        //lastPage.url += '/'+JSFunctions.
-        PageManager.dispatcher.dispatchEvent(PageManager.PAGE_CHANGED,targetPage);
+        for(var i = 0 ; i<pageData.length ; i++)
+        {
+            lastPage.url += '/'+encodeURIComponent(pageData[i]) ;
+        }
+        PageManager.dispatcher.dispatchEvent(PageManager.PAGE_CHANGED,lastPage);
     }
 }
 
-/**@return {SimplePage} */
+/**@description pass the constructor's props to the function and retrive parameters list */
+PageManager.decodePageParams = function(props={})
+{
+    if(
+        props !== undefined &&
+        props.match !== undefined && 
+        props.match.params !==null
+    )
+    {
+        var params = [] ;
+        for(var i = 0 ; i<maximomAcceptableParamsOnURL ; i++)
+        {
+            var par = props.match.params['p'+i] ;
+            if(par!==undefined)
+                params.push(decodeURIComponent(par));
+            else
+                break ;
+        }
+        return params ;
+    }
+}
+
+/**@return {PageData} */
 PageManager.getCurrentPage = function()
 {
     /**@type {string} */
