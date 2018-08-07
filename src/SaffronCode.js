bb00858618528c1505c5c65129571409cfd29c5d
MEbrahim Sepehr <ebrahimsepehr@gmail.com>
2018-08-07 08:49:58 +0430

    More libraries on NPM

--- src/SaffronCode.js
+++ src/SaffronCode.js
@@ -1,7 +1,11 @@
 import Preloader from './ui/PreLoader'
+import StringFunctions from './libs/StringFunctions';
 
 var SaffronCode = {};
 
+
+
 SaffronCode.Preloader = Preloader;
+SaffronCode.StringFunctions = StringFunctions ;
 
 export default SaffronCode ;
