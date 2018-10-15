f283922fc0845281dda9dd5019802070f4a21501
M.Ebrahim <ebrahimsepehr@gmail.com>
2018-10-15 07:55:28 +0330

    correctDates function
    This will update and debug your dates all over an object.

--- src/libs/JSFunctions.ts
+++ src/libs/JSFunctions.ts
@@ -1,9 +1,11 @@
 interface JSFunctionsModel {
-    objectToURIVars:typeof objectToURIVars
+    objectToURIVars:typeof objectToURIVars,
+    correctDates:typeof correctDates,
 }
 
 var JSFunctions:JSFunctionsModel = {
-    objectToURIVars:objectToURIVars
+    objectToURIVars:objectToURIVars,
+    correctDates:correctDates,
 } ;
 
 /**@description This function can convert your objecs to URIVariables string */
@@ -18,6 +20,35 @@ function objectToURIVars(obj:any={})
         str += key + "=" + encodeURIComponent(obj[key]);
     }
     return str ;
+}
+
+/**@description Pass any objects here and it will update all Date variables corrected with timeZoneOffsets. use full to correct api variables and sql variables. */
+function correctDates(obj:any):any
+{
+    for(var i in obj)
+    {
+        if(obj[i]!=null && typeof(obj[i]) === 'object')
+        {
+            if(obj[i].getTimezoneOffset !=undefined && obj[i].setMinutes != undefined && obj[i].getMinutes !=undefined)
+            {
+                //It  is date
+                obj[i].setMinutes(obj[i].getMinutes()-obj[i].getTimezoneOffset());
+            }
+            else
+            {
+                correctDates(obj[i]);
+            }
+        }
+        else if(obj[i]!=null && typeof(obj[i]) === 'string')
+        {
+            if(String(obj[i]).match(/[\d]{4}-[\d]{2}-[\d]{2}T[\d]{2}:[\d]{2}:[\d]{2}\.[\d]{3}[Z]?/)) 
+            {
+                obj[i] = new Date(obj[i]);
+                obj[i].setMinutes(obj[i].getMinutes()-obj[i].getTimezoneOffset());
+            }
+        }
+    }
+    return obj ;
 }
 
 export default JSFunctions ;
