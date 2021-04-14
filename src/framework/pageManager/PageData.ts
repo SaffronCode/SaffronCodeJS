b761e88738a28dd3d917e1d816191e509d18f4af
alirezafarin <alirafarinzad@gmail.com>
2021-04-14 11:25:45 +0430

    added  page params

--- src/framework/pageManager/PageData.ts
+++ src/framework/pageManager/PageData.ts
@@ -4,12 +4,14 @@ export default class PageData{
     pageName:string;
     component?:React.ComponentClass;
     pageData:any;
+    pageParams?:string[]
 
-    constructor(URL:string='',PageName:string='',Component?:React.ComponentClass)
+    constructor(URL:string='',PageName:string='', Component?:React.ComponentClass, PageParams?: string[])
     {
         this.url = URL ;
         this.pageName = PageName ;
         this.component = Component ;
         this.pageData = {} ;
+        this.pageParams = PageParams;
     }
 }
